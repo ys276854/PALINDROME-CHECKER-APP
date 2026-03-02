bcbca53bf599b923febcb4540ca1f5ef3bd1dc41
ys276854 <ys7981@srmist.edu.in>
2026-03-02 11:00:34 +0530

    UC11: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,31 @@
-public class Main {
-    public static void main(String[] args) {
-        String input = "A man a plan a canal Panama";
+class PalindromeService {
+    public boolean checkPalindrome(String str) {
+        String cleanStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        String cleanStr = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        if (cleanStr.isEmpty()) return true;
 
-        boolean isPalindrome = true;
         int left = 0;
         int right = cleanStr.length() - 1;
 
         while (left < right) {
             if (cleanStr.charAt(left) != cleanStr.charAt(right)) {
-                isPalindrome = false;
-                break;
+                return false;
             }
             left++;
             right--;
         }
+        return true;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String input = "racecar";
+
+        PalindromeService service = new PalindromeService();
+        boolean result = service.checkPalindrome(input);
 
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
     }
 }
