04dbeda721464b0ca4b9d29346fa2ac094691335
Yash Srivastava <ys7981@srmist.edu.in>
2026-03-10 08:59:31 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+public class UseCase1PalindromeCheckerApp {
 
-public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("=================================");
+        System.out.println("      PALINDROME CHECKER APP");
+        System.out.println("      Version 1.0.0");
+        System.out.println("=================================\n");
+
     }
 }
